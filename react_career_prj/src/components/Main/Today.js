64a2f45a7b4a669729e464348b4fc3e489423441
lhwn <cjsrnr1674@hanmail.net>
2021-04-10 09:46:28 +0900

    dynamic clock func

--- react_career_prj/src/components/Main/Today.js
+++ react_career_prj/src/components/Main/Today.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { makeStyles, Typography } from '@material-ui/core';
 import Link from '@material-ui/core/Link';
+import moment from 'moment';
 
 import Title from './Title';
 
@@ -16,19 +17,39 @@ const useStyles = makeStyles({
 });
 
 const Today = () => {
+  const [currentTime, setCurrentTime] = useState(moment()); // useState Hook 을 통해 timer 값 Default 설정
+  let timer = null;
+  useEffect(() => {
+    // timer 변수에 인터벌 종료를 위해 저장
+    timer = setInterval(() => {
+      setCurrentTime(moment()); // 현재 시간을 1000ms = 1s 마다 저장
+    }, 1000);
+    return () => {
+      clearInterval(timer); // 함수 unmount 시 clearInterval 해주기
+    };
+  }, []);
+
   const classes = useStyles();
   return (
     <React.Fragment>
       <Title>Today</Title>
-      <Typography component="p" variant="h4">
-        {/* dday 가져오기 */}D + 10
+      {/* 현재시각 : 시계처럼 만들기 */}
+      <Typography component="p" variant="h3" color="primary">
+        {currentTime.format('HH:mm:ss')}
+      </Typography>
+      <Typography component="p" variant="h5">
+        {/* 오늘 날짜 가져오기 */}
+        {'on '}
+        {moment().format('DD')}
+        {', '}
+        {['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'][moment().format('M') - 1]}
+        {', '}
+        {moment().format('YYYY')}
       </Typography>
       <Typography color="textSecondary" className={classes.depositContext}>
-        {/* 오늘 날짜 가져오기 */}
-        {'on 9, '}
-        {['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'][new Date().getMonth()]}
-        {' ,'}
-        {new Date().getFullYear()}
+        {/* dday 가져오기 */}
+        {'created Date D'}
+        {moment('2021-02-19').diff(moment().format('YYYY-MM-DD', 'days'), 'days')}
       </Typography>
       {/* 클릭 구현할거 생각하기 */}
       <div>
